f1f3d48cf4992a7723cc7bd7b6d25c2fd3e83481
Atul Varma <varmaa@gmail.com>
2015-07-19 19:29:37 -0400

    use 'import * as util' syntax in tests.

--- test/util.test.js
+++ test/util.test.js
@@ -1,12 +1,12 @@
-import {range} from "../src/util.js";
+import * as util from "../src/util.js";
 
-describe("range()", function() {
+describe("util.range()", function() {
   it("should work with one argument", function() {
-    range(3).should.eql([0, 1, 2]);
+    util.range(3).should.eql([0, 1, 2]);
   });
 
   it("should work with two arguments", function() {
-    range(3, 5).should.eql([3, 4]);
+    util.range(3, 5).should.eql([3, 4]);
   });
 });
 
