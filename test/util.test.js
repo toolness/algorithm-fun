10a22a3d6584513b47103d07144f219ee4580ad1
Atul Varma <varmaa@gmail.com>
2015-07-19 19:30:57 -0400

    use arrow syntax for tests.

--- test/util.test.js
+++ test/util.test.js
@@ -1,11 +1,11 @@
 import * as util from "../src/util.js";
 
-describe("util.range()", function() {
-  it("should work with one argument", function() {
+describe("util.range()", () => {
+  it("should work with one argument", () => {
     util.range(3).should.eql([0, 1, 2]);
   });
 
-  it("should work with two arguments", function() {
+  it("should work with two arguments", () => {
     util.range(3, 5).should.eql([3, 4]);
   });
 });
