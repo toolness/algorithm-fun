5a3cf8f751a49d1cc2b6c0cb5785c561b9bca9c3
Atul Varma <varmaa@gmail.com>
2015-08-24 11:55:44 -0400

    track entry/exit time in DFS

--- src/dfs/index.js
+++ src/dfs/index.js
@@ -4,6 +4,10 @@ class Node {
     this.parent = parent;
   }
 
+  childCount() {
+    return (this.exitTime - this.entryTime) / 2;
+  }
+
   toString() {
     return (this.parent ? (this.parent.toString() + ` -> ${this.vertex}`)
                         : this.vertex);
@@ -11,16 +15,27 @@ class Node {
 }
 
 function *dfs(graph, root, nodes = null, parent = null) {
-  nodes = nodes || {};
+  nodes = nodes || {__time: 0};
 
   nodes[root] = new Node(root, parent);
+  nodes[root].entryTime = ++nodes.__time;
 
-  yield nodes[root];
+  yield {
+    node: nodes[root],
+    phase: 'enter'
+  }
 
   for (let child of graph[root]) {
     if (!(child in nodes)) {
       yield *dfs(graph, child, nodes, nodes[root]);
     }
+  }
+
+  nodes[root].exitTime = nodes.__time++;
+
+  yield {
+    node: nodes[root],
+    phase: 'exit'
   }
 }
 
--- src/dfs/index.test.js
+++ src/dfs/index.test.js
@@ -1,13 +1,25 @@
 import dfs from "./index.js";
 
+const GRAPH = {
+  A: ['B', 'C'],
+  B: ['A', 'C'],
+  C: ['A', 'D'],
+  D: ['C']
+};
+
 describe("dfs", () => {
-  it("should work", () => {
-    let iter = dfs({
-      A: ['B', 'C'],
-      B: ['A', 'C'],
-      C: ['A', 'D'],
-      D: ['C']
-    }, 'A');
-    [...iter].map(node => node.vertex).should.eql(['A', 'B', 'C', 'D']);
+  it("should perform a depth-first traversal", () => {
+    [...dfs(GRAPH, 'A')]
+      .filter(entry => entry.phase == 'enter')
+      .map(entry => entry.node.vertex)
+      .should.eql(['A', 'B', 'C', 'D']);
+  });
+
+  it("should track entry/exit time and report child count", () => {
+    let iter = dfs(GRAPH, 'A');
+    let firstNode = iter.next().value.node;
+    firstNode.entryTime.should.eql(1);
+    while (!iter.next().done);
+    firstNode.childCount().should.eql(3);
   });
 });
